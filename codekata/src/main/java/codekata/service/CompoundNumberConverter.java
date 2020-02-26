a4ee3dbe9119917fdefa5979115b348e1eb9b44c
Yomi Ayodele <yomi.ayodele@ihsmarkit.com>
2020-02-26 18:44:44 +0000

    Add code for stage2

--- codekata/src/main/java/codekata/service/CompoundNumberConverter.java
+++ codekata/src/main/java/codekata/service/CompoundNumberConverter.java
@@ -1,5 +1,7 @@
 package codekata.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
@@ -7,13 +9,19 @@ import java.util.Set;
 
 public class CompoundNumberConverter implements NumberConverter {
     private final NumberConverter delegate;
-    private final Set<Integer> divisors = new HashSet<>();
+    private final List<Integer> divisors = new ArrayList<>();
+    private final List<String> listOfText = new ArrayList<>();
     private final String output;
 
     public CompoundNumberConverter(NumberConverter delegate, List<Integer> divisors, String output) {
+        this(delegate, divisors, Collections.emptyList(), output);
+    }
+
+    public CompoundNumberConverter(NumberConverter delegate, List<Integer> divisors, List<String> listOfText, String output) {
         Objects.requireNonNull(delegate, "Delegate transformer must be provided");
         Objects.requireNonNull(divisors, "divisors must be provided");
-        Objects.requireNonNull(output, "output transformer must be provided");
+        Objects.requireNonNull(output, "output must be provided");
+        Objects.requireNonNull(listOfText, "listOfText must be provided");
         if (divisors.isEmpty()) {
             throw new IllegalArgumentException(String.format("divisors cannot be empty"));
         }
@@ -29,6 +37,16 @@ public class CompoundNumberConverter implements NumberConverter {
 
     @Override
     public String convert(final Integer number) {
-        return divisors.stream().allMatch(divisor -> number % divisor == 0) ? output : delegate.convert(number);
+        return test(number) ? output : delegate.convert(number);
     }
+
+    private boolean test(Integer number) {
+        boolean canDivideBy = divisors.stream().allMatch(divisor -> number % divisor == 0);
+        if (listOfText.isEmpty()) {
+            return canDivideBy;
+        }
+        return canDivideBy || listOfText.stream().allMatch(text -> String.valueOf(number).contains(text));
+    }
+
+
 }
